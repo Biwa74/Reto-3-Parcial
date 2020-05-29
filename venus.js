87320234e3d1ceb5a2953a5ccb38048e7f0423c9
Biwa74 <60793045+Biwa74@users.noreply.github.com>
2020-05-29 11:23:46 -0600

    Create venus.js

---
+++ venus.js
@@ -0,0 +1,12 @@
+class Venus extends Planeta{
+  constructor(slider){
+    super();
+    this.x=2400;
+    this.y=2400;
+    this.trans=250;
+    this.angulo=120;
+    this.s=1/24;
+    this.p=loadImage("venus.png");
+    this.cont=0;
+  }
+}
